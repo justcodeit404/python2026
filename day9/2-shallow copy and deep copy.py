e64767951ad6dea1bcc19dfe4337ce80912b9700
孙彬彬 <16578052+binbin-sun@user.noreply.gitee.com>
2026-01-19 13:37:55 +0800

    day9

---
+++ day9/2-shallow copy and deep copy.py
@@ -0,0 +1,26 @@
+# 2026-01-19  13:28:46
+import copy
+def copy_use():
+
+    a=[1,2,3]
+    b=a.copy()
+    b[0]=10
+    print(a)
+    print(b)
+def copy_use2():
+    a=[1,2]
+    b=[3,4]
+    c=[a,b]
+    d=copy.copy(c)
+    print(id(c))
+    print(id(d))
+    print('-'*50)
+    a[0]=10
+    print(c)
+    print(d)
+    print(id(c[0][0]))
+    print(id(d[0][0]))
+
+
+if __name__ == '__main__':
+    copy_use2()
